e908841aa7bbb0315a183d42c64bfb66c76b8e8b
heyumar+ca <heyumar+ca@gmail.com>
2016-12-18 14:58:47 -0500

    added header and changed fonts

---
+++ wp-content/themes/first_theme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package first_theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'first_theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'first_theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'first_theme' ), 'first_theme', '<a href="http://underscores.me/" rel="designer">umar</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
